268781fde076e2de9659002653748baef1facc6d
BIRENDRA OLI <dev.birendraoli@gmail.com>
2024-04-01 22:11:08 +0545

    design added

--- news1/src/components/home-parts/Science.jsx
+++ news1/src/components/home-parts/Science.jsx
@@ -1,10 +1,76 @@
 import React from 'react'
 import './Science.scss'
+import img1 from './news-img1.jpg'
 
 const Science = () => {
-  return (
-    <div className='science-wrapper'>Science</div>
-  )
+    return (
+        <div className='science-latest'>
+            <div className='science-news'>
+                <div className='title-head'>
+                    <h1>Science</h1>
+                    <button>All</button>
+                </div>
+                <div className='news-content'>
+                    <div className='more-news'>
+                        <div className="col">
+                            <img src={img1} alt="" />
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                        <div className="col">
+                            <img src={img1} alt="" />
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                        <div className="col">
+                            <img src={img1} alt="" />
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                        <div className="col">
+                            <img src={img1} alt="" />
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                        <div className="col">
+                            <img src={img1} alt="" />
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                    </div>
+                    <div className='banner-news'>
+                        <div className="title">
+                            <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        </div>
+                        <div className="feature-img">
+                            <img src={img1} alt="" />
+                        </div>
+                        <div className="details">
+                            <p>१९ चैत, काठमाडौं । रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै सिंहदरबार अगाडि प्रदर्शन गर्नेहरुलाई प्रहरीले नियन्त्रणमा लिएको छ । रुसी सेनामा रहेका नागरिकको जीवन रक्षाको लागि सचेतना...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='latest-news'>
+                <div className="title-head">
+                    <h1>Latest Update</h1>
+                </div>
+                <div className="news-content">
+                    <div className="col">
+                        <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        <img src={img1} alt="" />
+                    </div>
+                    <div className="col">
+                        <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        <img src={img1} alt="" />
+                    </div>
+                    <div className="col">
+                        <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        <img src={img1} alt="" />
+                    </div>
+                    <div className="col">
+                        <h2>रुसी सेनामा कार्यरत नेपालीको जीवन रक्षाको माग गर्दै प्रदर्शन गर्नेहरु पक्राउ</h2>
+                        <img src={img1} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Science
